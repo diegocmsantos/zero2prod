86e65972cb8534a168bb97b508574b934e5da9ea
Diego Maia <diego.maia@emma-sleep-.com>
2024-12-10 17:38:45 +0000

    Chapter to deploy in digital ocean

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# Builder stage
+FROM rust:1.83.0 AS builder
+WORKDIR /app
+RUN apt-get update && apt-get install lld clang -y
+COPY . .
+ENV SQLX_OFFLINE=true
+RUN cargo build --release
+# Runtime stage
+FROM rust:1.83.0-slim AS runtime
+WORKDIR /app
+# Copy the compiled binary from the builder environment
+# to our runtime environment
+COPY --from=builder /app/target/release/zero2prod zero2prod
+# We need the configuration file at runtime!
+COPY configuration configuration
+ENV APP_ENVIRONMENT=production
+ENTRYPOINT ["./zero2prod"]
--- src/configuration.rs
+++ src/configuration.rs
@@ -4,8 +4,9 @@ use secrecy::{ExposeSecret, SecretString};
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16,
+    pub application: ApplicationSettings,
 }
+
 #[derive(serde::Deserialize)]
 pub struct DatabaseSettings {
     pub username: String,
@@ -15,8 +16,69 @@ pub struct DatabaseSettings {
     pub database_name: String,
 }
 
+#[derive(serde::Deserialize)]
+pub struct ApplicationSettings {
+    pub port: u16,
+    pub host: String,
+}
+
+// the possible runtime environment for our application.
+pub enum Environment {
+    Local,
+    Production,
+}
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Local => "local",
+            Environment::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "{} is not a supported environment. Use either 'local' or 'production'.",
+                other
+            )),
+        }
+    }
+}
+
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
-    let builder = Config::builder().add_source(config::File::with_name("configuration"));
+    // let builder = Config::builder().add_source(config::File::with_name("configuration"));
+
+    let base_path = std::env::current_dir().expect("Failed to determine the current directory");
+    let configuration_directory = base_path.join("configuration");
+
+    // Detect the running enviroment.
+    // Default to "local" if unspecified.
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT");
+
+    // Read the "default" configuration
+    let builder = Config::builder()
+        .add_source(
+            config::File::with_name(configuration_directory.join("base").to_str().unwrap())
+                .required(true),
+        )
+        .add_source(
+            config::File::with_name(
+                configuration_directory
+                    .join(environment.as_str())
+                    .to_str()
+                    .unwrap(),
+            )
+            .required(true),
+        );
 
     match builder.build() {
         Ok(config) => {
