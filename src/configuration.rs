8abacc1e1ff224a2494dbda1ebf30d6df2d11760
Diego Maia <diego.maia@emma-sleep-.com>
2024-12-12 12:07:32 +0000

    Prefix separator

--- src/configuration.rs
+++ src/configuration.rs
@@ -88,7 +88,7 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
         )
         .add_source(
             config::Environment::with_prefix("APP")
-                .prefix("_")
+                .prefix_separator("_")
                 .separator("__"),
         );
 
